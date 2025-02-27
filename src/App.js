75bf748d8b36c3c2fdced680287eaf9e2b9af042
Aliza Razi <aliza302001@gmail.com>
2025-02-27 15:26:22 +0530

    Refactor user management routes and add email existence check, user deletion, and update functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ const app = express();
 const connectDB = require('./config/Database');
 const user = require("./Models/user");
 
+connectDB()
+    .then(() => {
+        console.log('Database connected');
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000');
+        });
+
+    }).catch((err) => {
+        console.log('Error connecting to database');
+    });
+
 
 app.use(express.json());
 app.post("/signup", async (req, res) => {
@@ -25,26 +36,91 @@ app.get("/byEmail", async (req, res) => {
     const userEmail = req.body.email;
     try {
         const UserData = await user.findOne({ email: userEmail });
-        res.send(UserData);
+        if (!UserData) {
+            res.status(401).send("User not found");
+        }
+        else {
+            res.send(UserData);
+        }
     } catch (err) {
-        res.send(err.message)
+        res.status(400).send(err.message)
         console.error(err);
     }
 
 })
-
-
-app.get("/feed", (req, res) => {
+// all the Data 
+app.get("/all", async (req, res) => {
+    try {
+        const UserData = await user.find({});
+        res.send(UserData);
+    } catch (err) {
+        res.status(400).send(err.message)
+        console.error(err);
+    }
 
 })
+app.get("/oneMail", async (req, res) => {
+    const userEmail = req.body.email;
+    try {
+        console.log(userEmail)
+        const UserData = await user.findOne({ email: userEmail });
+        if (!UserData) {
+            res.status(401).send("User not found");
+        }
+        else {
+            res.send(UserData);
+        }
+        res.send(UserData);
+    } catch (err) {
+        res.status(400).send(err.message)
+        console.error(err);
+    }
+})
 
-connectDB()
-    .then(() => {
-        console.log('Database connected');
-        app.listen(3000, () => {
-            console.log('Server is running on port 3000');
-        });
+app.get("/mailExist", async (req, res) => {
+    const mail = req.body.email;
+    try {
+        const UserData = await user.exists({ email: mail });
+        if (UserData) {
+            res.send("Email Exist");
+        }
+        else {
+            res.send("Email not Exist");
+        }
+    }
+    catch (err) {
+        res.status(400).send(err.message)
+        console.error(err);
+    }
+})
 
-    }).catch((err) => {
-        console.log('Error connecting to database');
-    });
+app.delete("/delete", async (req, res) => {
+    const userId = req.body.userID;
+    try {
+        const UserData = await user.findOneAndDelete({ userId });
+        res.send("User Deleted");
+    }
+    catch (err) {
+        res.status(400).send(err.message)
+        console.error(err);
+    }
+});
+
+app.patch("/update", async (req, res) => {
+    const userId = req.body.userID; // Ensure userID is provided
+    const updateData = req.body;
+
+    console.log(updateData, userId);
+    try {
+        const UserData = await user.findByIdAndUpdate(userId, updateData, { returnDocument: "after" });
+        res.send("User updated successfully");
+    } catch (err) {
+        res.status(400).send(err.message);
+        console.error(err);
+    }
+})
+
+
+
+
+
